53cdecda4b87e72da77724042e9add15644507e6
mashoun <mashoun.me@gmail.com>
2022-04-09 00:00:05 +0300

    done

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-// test 1
-// test 2
+var logo = document.getElementById('logo')
+var profile = document.getElementById('profile')
+var email = document.getElementById('email')
+var api = 'https://script.google.com/macros/s/AKfycbwKM0cnkmznPn74VgV3sWIAW2ZakuDmGZwNtmSeBT8KxSI5JnEMGxjctnfRFI22ViDFww/exec'
+
+//promise
+fetch(api).then( res => res.json()).then(function(res){
+    console.log(res)
+    logo.src = res.pic
+    profile.innerText = res.name
+    email.href = `mailto:${res.email}`
+})
