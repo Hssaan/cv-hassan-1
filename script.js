caa967abb271a8294846aee63d2b542529e009e7
mashoun <mashoun.me@gmail.com>
2022-04-09 00:15:42 +0300

    access

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var logo = document.getElementById('logo')
 var profile = document.getElementById('profile')
 var email = document.getElementById('email')
-var api = 'https://script.google.com/macros/s/AKfycbwKM0cnkmznPn74VgV3sWIAW2ZakuDmGZwNtmSeBT8KxSI5JnEMGxjctnfRFI22ViDFww/exec'
+var api = 'https://script.google.com/macros/s/AKfycbzgfRvd1I_WXDOxz-CyQ4sjdI9p6UbFIn-Pa15A1ii7SG28N5JnbEO_Pr3UyxnqHdkJ/exec'
 
 //promise
 fetch(api).then( res => res.json()).then(function(res){
